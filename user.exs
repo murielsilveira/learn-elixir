ccac9192e17583bb49a432f8796ab67c7193c987
Muriel Silveira <murielkong@gmail.com>
2017-01-16 00:00:20 +0100

    Remove docs

--- user.exs
+++ user.exs
@@ -1,44 +1,15 @@
 defmodule User do
-  @moduledoc """
-  User struct and functions to handle user related operations
-  """
   import String, only: [split: 1]
   import List, only: [first: 1, last: 1]
 
   defstruct name: nil, email: nil
 
-  @doc """
-  Get user's first name
-
-  ## Parameters
-
-  - `user`: a User struct
-
-  ## Examples
-
-      user = %User{name: "John Doe"}
-      User.first_name(user)
-      "John"
-  """
   def first_name(user) do
     user.name
     |> split
     |> first
   end
 
-  @doc """
-  Get user's last name
-
-  ## Parameters
-
-  - `user`: a User struct
-
-  ## Examples
-
-      user = %User{name: "John Doe"}
-      User.last_name(user)
-      "Doe"
-  """
   def last_name(user) do
     user.name
     |> split
